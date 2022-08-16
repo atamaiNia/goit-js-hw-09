f786906ee7f3cbcc30f0194d63443bc8473e3f30
atamaiNia <bimatideka@gmail.com>
2022-08-16 14:34:06 +0300

    fix task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -56,31 +56,29 @@ function onSubmitForm(e) {
   let step = Number(e.target.step.value);
   let amount = Number(e.target.amount.value);
 
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
+
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        setTimeout(() => {
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        }, delay);
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
 
       .catch(({ position, delay }) => {
-        setTimeout(() => {
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        }, delay);
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
     delay += step;
   }
-}
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-
-  return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
-  });
 }
